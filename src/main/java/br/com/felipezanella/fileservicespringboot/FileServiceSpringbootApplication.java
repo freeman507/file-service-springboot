3553669b45947ad13df08c9338c3f69a2c7644f5
Felipe Zanella <freeman.zanella@gmail.com>
2020-06-02 10:33:44 -0300

    first commit

---
+++ src/main/java/br/com/felipezanella/fileservicespringboot/FileServiceSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipezanella.fileservicespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileServiceSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileServiceSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/felipezanella/fileservicespringboot/FileServiceSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipezanella.fileservicespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileServiceSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
